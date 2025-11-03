606660bbb1bf0854cf37c5aa0bd8e25ff09a08bf
daniedu <81932800+daniedu@users.noreply.github.com>
2025-11-02 19:58:30 -0500

    updating page with base functionality

---
+++ _next/static/chunks/c5deb814ab087cd6.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/11314f78ae5aa486.js",
+  "static/chunks/58f2e14717793a7d.js",
+  "static/chunks/turbopack-c42e491154cb33fb.js"
+])
---
+++ _next/static/chunks/d3ee8453db60051f.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c0a5377b3e5f35ec.js",
+  "static/chunks/58f2e14717793a7d.js",
+  "static/chunks/turbopack-7e6042771276c1ed.js"
+])
